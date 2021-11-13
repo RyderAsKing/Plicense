9a65361358036370321c114c946828ee11a1df11
RyderAsking <clasherlodt@gmail.com>
2021-11-13 18:54:32 +0530

    Licenses table

---
+++ database/migrations/2021_11_13_132148_create_license_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLicenseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('license', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('key');
+            $table->string('ip');
+            $table->date('expires_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('license');
+    }
+}
