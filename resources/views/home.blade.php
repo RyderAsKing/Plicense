03dae12c7bcf55f6ede0db3d3cb926d8fc2c0601
RyderAsking <clasherlodt@gmail.com>
2021-11-13 21:14:04 +0530

    Dashboard looks

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,56 @@
 @stop
 
 @section('content')
-<p>Welcome to this beautiful admin panel.</p>
+<div class="row">
+    @if(Auth::user()->type == 1)
+    <!-- ./col -->
+    <div class="col-lg-6 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>44</h3>
+
+                <p>Total User</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-users"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-6 col-6">
+        <!-- small box -->
+        <div class="small-box bg-danger">
+            <div class="inner">
+                <h3>65</h3>
+
+                <p>Total Licenses</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-ticket-alt"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    @else
+    <div class="col-lg-12 col-12">
+        <!-- small box -->
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>53</h3>
+
+                <p>Your Licenses</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-ticket-alt"></i>
+            </div>
+        </div>
+    </div>
+    @endif
+
+</div>
 @stop
 
 @section('css')
