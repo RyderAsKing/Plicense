19db23ba7cdcd9403e5b0e7fad60161add33713c
RyderAsking <clasherlodt@gmail.com>
2021-11-13 21:18:59 +0530

    Update

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('home');
+});
+
 Auth::routes();
 
 Route::group(['middleware' => ['auth']], function () {
-    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
