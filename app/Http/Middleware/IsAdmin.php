181f31e4c3575b3d0c27869da1271d2d9049c309
RyderAsking <clasherlodt@gmail.com>
2021-11-13 19:03:01 +0530

    Is admin middleware

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (!Auth::check()) {
+            return redirect()->route('login');
+        }
+
+        if (Auth::user()->type == 1) {
+            return $next($request);
+        }
+
+        return redirect()->back()->with('message', 'You are 
+                  unauthorised to access this page');
+    }
+}
