6ead689d6ffbfb03f614e7be0629e4f82c2d9f42
RyderAsking <clasherlodt@gmail.com>
2021-11-13 20:23:17 +0530

    Admin LTE dashboard setup

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
