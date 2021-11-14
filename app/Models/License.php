f398f5393edbff08ea1cdc967d582e52e63cc9f9
RyderAsking <clasherlodt@gmail.com>
2021-11-14 09:32:07 +0530

    License model

---
+++ app/Models/License.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class License extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'key',
+        'ip',
+        'expires_at',
+    ];
+}
