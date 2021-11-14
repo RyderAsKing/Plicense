8e9c9e11a20e011f8cd5dd9d2411fcf59990a223
RyderAsking <clasherlodt@gmail.com>
2021-11-14 11:48:35 +0530

    Right sidebar removed

--- config/adminlte.php
+++ config/adminlte.php
@@ -167,7 +167,7 @@ return [
     |
     */
 
-    'right_sidebar' => true,
+    'right_sidebar' => false,
     'right_sidebar_icon' => 'fas fa-cogs',
     'right_sidebar_theme' => 'dark',
     'right_sidebar_slide' => true,
