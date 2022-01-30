4f00333016fd2e93900ed31953db15125bc7ecdf
RyderAsking <clasherlodt@gmail.com>
2022-01-30 15:44:06 +0530

    Added api token update route

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ApiController extends Controller
+{
+    //
+    public function update(Request $request)
+    {
+        //
+        $token = Str::random(32);
+        $request->user()->forceFill([
+            'api_token' => hash('sha256', $token),
+        ])->save();
+        return back()->with('token', $token);
+    }
+}
--- config/auth.php
+++ config/auth.php
@@ -40,7 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
-        'api' => ['driver' => 'token', 'provider' => 'users']
+        'api' => ['driver' => 'token', 'provider' => 'users', 'hash' => true,]
     ],
 
     /*
